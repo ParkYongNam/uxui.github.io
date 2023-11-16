c171a47d15f038cf6682cfff5f471d6c537050ba
haruka135 <harune135@naver.com>
2023-11-16 22:42:45 +0900

    사이즈전까지

---
+++ js/web.js
@@ -0,0 +1,44 @@
+$('document').ready(function(){
+    $('.lang').click(function(){
+        $('.l-menu').toggleClass('on');
+    });
+
+    $('.l-menu>li>a').mouseover(function(){
+        $(this).addClass('checked').parent().siblings().find('a').removeClass('checked');
+    });
+
+    $('#gnb>li').mouseover(function(){
+        $('.back').addClass('cover');
+        $('.s-menu').addClass('son');
+    });
+
+    $('#gnb>li').mouseleave(function() {
+        $('.back').removeClass('cover');
+        $('.s-menu').removeClass('son');
+    });
+
+    var swiper = new Swiper('.swiper#new_swi', {
+
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+      });
+
+    $('.city>li').click(function(){
+        $(this).toggleClass('sign').siblings().removeClass('sign');
+    });
+
+    $('.subcity>li').click(function(){
+        $(this).toggleClass('gra').siblings().removeClass('gra');
+    });
+});
