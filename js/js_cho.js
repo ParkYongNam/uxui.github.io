a88d60a292f0b1cceec6d5c060e6a35146f1250d
User -2023EMGRM\Administrator <zxc770629@gmail.com>
2023-11-20 17:37:13 +0900

    main_포토리뷰_오류수정_11/20

--- js/js_cho.js
+++ js/js_cho.js
@@ -1,24 +1,27 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
+    showPopup();
+
     const swiper = new Swiper('#reviewBanner .swiper', {
         loop: true,
         effect: 'fade',
         autoplay: {
             delay: 4000
         },
-    
-   /*      pagination: {
-            el: '#reviewBanner .swiper-pagination',
-            type: 'fraction'
-        }, */
-    
+
+        /*      pagination: {
+                 el: '#reviewBanner .swiper-pagination',
+                 type: 'fraction'
+             }, */
+
         navigation: {
             nextEl: '#reviewBanner .swiper-button-next',
             prevEl: '#reviewBanner .swiper-button-prev',
         },
-    
+
     });
 
-    
+
 })
 
 //     팝업 광고
@@ -31,12 +34,6 @@ function closePopup() {
     document.querySelector('.popup').style.display = 'none';
     document.querySelector('.background').style.display = 'none';
 }
-
-// 페이지 로드 시 팝업 표시
-window.onload = function() {
-    showPopup();
-};
-
 
 
 
