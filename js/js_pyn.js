a2f5421901e9f9e25f2c5001b1452fb23210f3fa
User -2023VHEIP\Administrator <dkskcka3@naver.com>
2023-11-23 18:05:55 +0900

    악

--- js/js_pyn.js
+++ js/js_pyn.js
@@ -519,15 +519,15 @@ $(function () {
     ]
 
 
-    var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-    var options = { //지도를 생성할 때 필요한 기본 옵션
-        center: new kakao.maps.LatLng(mapinfo[infonum].subcity[0].mapinfo[0], mapinfo[infonum].subcity[0].mapinfo[1]), //지도의 중심좌표.
-        level: 3 //지도의 레벨(확대, 축소 정도)
-    };
+    /*  var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+     var options = { //지도를 생성할 때 필요한 기본 옵션
+         center: new kakao.maps.LatLng(mapinfo[infonum].subcity[0].mapinfo[0], mapinfo[infonum].subcity[0].mapinfo[1]), //지도의 중심좌표.
+         level: 3 //지도의 레벨(확대, 축소 정도)
+     };
+  */
 
-
-
-    var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    /* 
+        var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴 */
 
 
     for (x in mapinfo) {
